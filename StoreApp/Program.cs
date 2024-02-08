a883c38315d3030725e9fc12662fdf810b0bfe32
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-02-08 12:34:17 +0300

    First Commit
    İlk committe boş bir web projesini cmd üzerinden MVC template haline getirdik. Program.cs'de gerekli servisleri yaptık.
    .csproj uzantılı dosyada görülmek üzere paket yükledik.Bunu da dotnet CLI ile yaptık.

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default", 
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

374af5c84c847b7429e8c2873aa107afb2240af0
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-03-09 00:34:41 +0300

    Dto başlangıcı
    Bu kısımda Dto oluşturup validation işlemlerini orada yaptık. Sonraki aşamada bu dto'yu servislere bağlayıp websitemizde göreceğiz.

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -4,16 +4,8 @@ namespace Entities.Models;
 public class Product
     {
         public int Id { get; set; }
-        
-        [Required(ErrorMessage ="Product Name is required")]
         public String? ProductName { get; set; }=String.Empty;
-
-         [Required(ErrorMessage ="Price is required")]
         public decimal Price { get; set; }
-
         public int? CategoryId{get; set;} //Foreign Key
-        
-        public Category? Category{get; set;} //Navigation property
-
-        
+        public Category? Category{get; set;} //Navigation property        
     }

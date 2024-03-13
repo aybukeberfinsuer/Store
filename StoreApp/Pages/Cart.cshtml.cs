7e7922fade133206e674e1ffd4f9f39d3bb493ea
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-03-13 12:59:48 +0300

    Cart bilgilerinin düzenlenmesi

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -1,9 +1,42 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Services.Contracts;
 
 namespace StoreApp.Pages{
 
-    public class CardModel:PageModel
+    public class CartModel:PageModel
     {
-        
+        private readonly IServiceManager _manager;
+
+        public CartModel(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public Cart Cart { get; set; } //IoC yapıyoruz
+
+        public string  ReturnUrl { get; set; } ="/";
+
+        public void OnGet(string returnUrl){
+            ReturnUrl= returnUrl ?? "/";
+        }
+
+        public IActionResult OnPost(int Id,string returnUrl)
+        {
+            Product? product= _manager.ProductService.GetOneProduct(Id,false);
+
+            if(product is not null){
+                Cart.AddItem(product,1);
+            }
+            return Page(); //returnUrl logici yönlendireceğiz.
+            
+        }
+
+        public IActionResult OnPostRemove(int Id,string returnUrl){
+            Cart.RemoveLine(Cart.Lines.First(cl=>cl.Product.Id.Equals(Id)).Product);
+            return Page();
+        }
     }
 }

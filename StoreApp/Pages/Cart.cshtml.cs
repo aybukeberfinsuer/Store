e260404c31a743270a03067616555e8eec967764
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-03-22 11:00:41 +0300

    HttpRequest Extensions yazdık

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -5,6 +5,10 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Services.Contracts;
 using StoreApp.Infrastructure.Extensions;
 
+// Yorum satırına aldığım yerleri (-A yazdığım yerler) SessionCart.cs Modelinde ekleme çıkarma işlemi yaparken session ile uyumlu hale getirdim. bknz:StoreApp/Models/SessionCart.cs
+// Bu sebeple burada tekrardan yazmama gerek kalmıyor.Fakat o sınıfı yazmasaydık nasıl olurdu ? Sorusunun yanıtı için satırları silmedim. 
+// Sonuç olarak SessionCart.cs sınıfının yazımı zorunlu değil fakat kod tekrarlılığını azaltı okunabilirliği artırmak amacıyla yazılması faydalı olacaktır.
+
 namespace StoreApp.Pages
 {
 
@@ -25,7 +29,7 @@ namespace StoreApp.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-          //  Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+          //  Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();-A
             //varsa olan cart nesnesini yoksa yeni bir Cart nesnesi oluşturfuk
         }
 
@@ -36,11 +40,13 @@ namespace StoreApp.Pages
             if (product is not null)
             {
 
-              //  Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+              //  Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();-A
                 Cart.AddItem(product, 1);
-              //  HttpContext.Session.SetJson<Cart>("cart", Cart);
+              //  HttpContext.Session.SetJson<Cart>("cart", Cart);-A
             }
-            return Page(); //returnUrl logici yönlendireceğiz.
+            return RedirectToPage(new{
+              returnUrl=returnUrl
+            }); 
 
         }
 
@@ -48,10 +54,11 @@ namespace StoreApp.Pages
         {
 
 
-        // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart(); -A
             Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.Id.Equals(Id)).Product);  
-         // HttpContext.Session.SetJson<Cart>("cart", Cart);
+         // HttpContext.Session.SetJson<Cart>("cart", Cart); -A
             return Page();
         }
     }
 }
+

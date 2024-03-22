19442a89ed531a6a762296419c683384da069f17
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-03-22 10:04:31 +0300

    Cart'a component eklenmesi

---
+++ StoreApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Components{
+    
+    public class CartSummaryViewComponent:ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public string Invoke(){
+
+            return _cart.Lines.Count().ToString();
+        }
+    }
+
+}
